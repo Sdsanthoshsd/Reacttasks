67f367274e9e7ab2ac4972253fb029445acea20c
Sdsanthoshsd <114224208+Sdsanthoshsd@users.noreply.github.com>
2023-08-16 20:54:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CounterApp from "./Tasks/Counter";
+
+function App() {
+  return (
+    <div>
+      <CounterApp/>
+   
+    </div>
+  );
+
+  
+}
+
+export default App;
